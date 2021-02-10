c854eec36f3f5d5dd43d79453e84a3f4ced98e5a
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-02-11 01:01:13 +0400

    01.11.2021

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Core.Utilities;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data,bool success, string message):base(success,message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success):base(success)
+        {
+            Data = data;
+        }
+       
+        public T Data { get; }
+    }
+}
