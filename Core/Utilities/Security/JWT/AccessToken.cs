33d3638227a1cd7addc7628701ab076317b6b324
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-02-28 02:54:15 +0400

    14.Ders

--- Core/Utilities/Security/JWT/AccessToken.cs
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Utilities.Security.JWT
 {
-    public class AccessToken:ITokenHelper
+    public class AccessToken
     {
         public string Token { get; set; }
         public DateTime Expiration { get; set; }
+
     }
 }
